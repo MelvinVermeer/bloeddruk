b0546681fbcd40a1aba53fb4c9ae7f0f5c5105db
Melvin Vermeer <melvin.vermeer@gmail.com>
2020-11-26 20:13:44 +0100

    title duncan

--- app/src/App.js
+++ app/src/App.js
@@ -4,6 +4,7 @@ import { MyMapComponent } from "./components/map/MyMapComponent";
 function App() {
   return (
     <div className="App">
+      <h1>HOI DUNCAN</h1>
       <MyMapComponent
         googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAxEr3-8MbFi6ckVecXQRcf9S3fVKIYazM&v=3.exp&libraries=geometry,drawing,places"
         loadingElement={<div style={{ height: `100%` }} />}
